e8662451d43c8c1f33ab4b921f9fa442ce4f0ead
Julien Dessaux <julien.dessaux@adyxax.org>
2021-09-19 01:14:58 +0200

    Implemented function to get a field value

--- pkg/field/utils.go
+++ pkg/field/utils.go
@@ -1,5 +1,15 @@
 package field
 
+func (f Field) Get(x, y int) int {
+	if y >= f.y && y < f.y+f.ly {
+		l := f.lines[y-f.y]
+		if x >= l.x && x < l.x+l.l {
+			return l.columns[x-l.x]
+		}
+	}
+	return ' '
+}
+
 func (f Field) isIn(x, y int) bool {
 	return x >= f.x && x < f.x+f.lx && y >= f.y && y < f.y+f.ly
 }
--- pkg/field/utils_test.go
+++ pkg/field/utils_test.go
@@ -7,6 +7,39 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestGet(t *testing.T) {
+	// Test cases
+	testCases := []struct {
+		name     string
+		input    string
+		inputX   int
+		inputY   int
+		expected int
+	}{
+		{"minimal0,0", "test_data/minimal.b98", 0, 0, '@'},
+		{"minimal-1,0", "test_data/minimal.b98", -1, 0, ' '},
+		{"minimal1,0", "test_data/minimal.b98", 1, 0, ' '},
+		{"minimal0,-1", "test_data/minimal.b98", 0, -1, ' '},
+		{"minimal0,1", "test_data/minimal.b98", 0, 1, ' '},
+		{"hello3,0", "test_data/hello.b98", 3, 0, '"'},
+		{"hello3,1", "test_data/hello.b98", 3, 1, ' '},
+		{"factorial0,1", "test_data/factorial.b98", 0, 1, ' '},
+		{"factorial14,1", "test_data/factorial.b98", 14, 1, ' '},
+		{"factorial15,1", "test_data/factorial.b98", 15, 1, ' '},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			file, err := os.Open(tc.input)
+			require.NoError(t, err, "Failed to open file")
+			defer file.Close()
+			field, err := LoadFile(file)
+			valid := field.Get(tc.inputX, tc.inputY)
+			require.NoError(t, err)
+			require.Equal(t, tc.expected, valid, "Invalid value")
+		})
+	}
+}
+
 func TestIsIn(t *testing.T) {
 	// Test cases
 	testCases := []struct {
