ea72b9dbb21ebd249b84bdb17d34cf70d14daf1b
Julien Dessaux <julien.dessaux@adyxax.org>
2021-09-22 23:34:59 +0200

    Finished implementing the stack stack

--- pkg/stack/stack-stack.go
+++ pkg/stack/stack-stack.go
@@ -43,3 +43,49 @@ func (ss *StackStack) Begin(p *pointer.Pointer) {
 	soss.Push(y)
 	p.CalculateNewStorageOffset()
 }
+
+func (ss *StackStack) End(p *pointer.Pointer) (reflect bool) {
+	if ss.height == 1 {
+		return true
+	}
+	soss := ss.head.next
+	n := ss.head.Pop()
+	y := soss.Pop()
+	x := soss.Pop()
+	p.SetStorageOffset(x, y)
+	if n > 0 {
+		soss.height += n
+		if soss.size < soss.height {
+			soss.data = append(soss.data, make([]int, soss.height-soss.size)...)
+			soss.size = soss.height
+		}
+		for i := n; i > 0; i-- {
+			soss.data[soss.height-i] = ss.head.data[ss.head.height-i]
+		}
+	} else {
+		soss.height += n
+		if soss.height < 0 {
+			soss.height = 0
+		}
+	}
+	ss.height--
+	ss.head = ss.head.next
+	return false
+}
+
+func (ss *StackStack) Under() (reflect bool) {
+	if ss.height == 1 {
+		return true
+	}
+	n := ss.head.Pop()
+	if n > 0 {
+		for i := 0; i < n; i++ {
+			ss.head.Push(ss.head.next.Pop())
+		}
+	} else {
+		for i := 0; i < -n; i++ {
+			ss.head.next.Push(ss.head.Pop())
+		}
+	}
+	return false
+}
